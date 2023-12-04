d19ac3a3c74b73a63bc52caec24411f28a899acb
Falk B. <86295521+falk234@users.noreply.github.com>
2023-12-04 20:07:10 +0100

    Add playGame function to determine winner

--- js/script.js
+++ js/script.js
@@ -11,15 +11,21 @@ let computerChoiceString;
 
 // Translate random number to rock, paper or scissor
 
-if (computerChoice===1){
-    computerChoiceString="rock";
-}else if (computerChoice===2){
-    computerChoiceString="paper";
-}else if (computerChoice===3){
-    computerChoiceString="scissor";
-}else{
-    alert("Fehler: Es wurde eine falsche Zahl generiert!")
+function computerChoiceIntToString(computerChoice){
+    if (computerChoice===1){
+        return "rock";
+    }else if (computerChoice===2){
+        return "paper";
+    }else if (computerChoice===3){
+        return "scissor";
+    }else{
+        alert("Fehler: Es wurde eine falsche Zahl generiert!")
+    }
 }
+
+computerChoiceString=computerChoiceIntToString(computerChoice);
+
+
 
 // Show decision of computer
 
@@ -30,16 +36,53 @@ console.log("Der Computer hat sich für \"" + computerChoiceString + "\" entschi
 let playerSelectionString = prompt("What is your choice?", "Rock | Paper | Scissors");
 playerSelectionString= playerSelectionString.toLowerCase();
 
-console.log("playerselection: " + playerSelectionString);
+console.log("Der Spieler hat sich für \"" + playerSelectionString + "\" entschieden");
+
+
+// Translate playerSelection string to Int
 
 let playerSelection;
 
-if (playerSelectionString==="rock"){
-    playerSelection=1;
-}else if (playerSelectionString==="paper"){
-    playerSelection=2;
-}else if (playerSelectionString==="scissor"){
-    playerSelection=3;
-}else{
-    alert('Fehler: "' + playerSelectionString + '" ist keine gültige Eingabe.\n\nNur "Schere", "Stein" oder "Papier" sind erlaubt');
+function playerSelectionStringToInt(playerSelectionString){
+    if (playerSelectionString==="rock"){
+        return 1;
+    }else if (playerSelectionString==="paper"){
+        return 2;
+    }else if (playerSelectionString==="scissor"){
+        return 3;
+    }else{
+        alert('Fehler: "' + playerSelectionString + '" ist keine gültige Eingabe.\n\nNur "Schere", "Stein" oder "Papier" sind erlaubt');
+        return undefined;
+    }
 }
+
+// Play game to get winner
+    // player + computer => 
+    // rock + paper => loose
+    // rock + scissor => win
+    // rock + rock => ties
+
+    // paper + rock => win
+    // paper + scissor => loose
+    // paper + paper => ties
+
+    // scissor + rock => loose
+    // scissor + paper => win
+    // scissor + scissor => ties
+
+playerSelection=playerSelectionStringToInt(playerSelectionString);
+
+function play_game(computerChoiceString,playerSelectionString){
+    if ((playerSelectionString==="rock") && (computerChoiceString==="paper") || (playerSelectionString==="paper") && (computerChoiceString==="scissor") || (playerSelectionString==="scissor") && (computerChoiceString==="rock")){
+        return ('You lose! ' + computerChoiceString + ' beats ' + playerSelectionString);
+    }else if((playerSelectionString==="rock") && (computerChoiceString==="scissor") || (playerSelectionString==="paper") && (computerChoiceString==="rock") || (playerSelectionString==="scissor") && (computerChoiceString==="paper")){
+        return ('You win! ' + playerSelectionString + ' beats ' + computerChoiceString);
+    }else if(playerSelection===computerChoice){
+        return "You both chose the same, try again!"
+    }else{
+        return "Diese Möglicheit ist noch nicht implementiert.";
+    }
+}
+
+
+console.log(play_game(computerChoiceString,playerSelectionString));
