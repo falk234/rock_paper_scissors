ae3f28df12b079b2d89afeac8b56b07c73d1adff
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-08 15:59:29 +0100

    Display running score, announce winner once one player reaches set points.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let rounds=5;
+let currentRound=1;
+let playerPoints=0;
+let computerPoints=0;
+
+const buttons=document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', btnEvent => {
+        playGame(btnEvent.target.id);
+    }) 
+});
+
+// const button_rock=document.querySelector('#rock');
+// button_rock.addEventListener('click', () => {playGame('rock')});
+
+
 function getComputerChoice(){
 // COMPUTE random number 1-3
     let number = Math.floor(Math.random() * 3) +1;
@@ -8,10 +24,7 @@ function getComputerChoice(){
         case 3: return "scissor";
     }
 }
-// TEST
-// console.log(getComputerChoice());
 
-// FUNCTION determineGameDecision (computerChoice,playerChoice)
 function determineGameDecision(computerChoice,playerChoice){
     let playerWin=(computerChoice === "rock" && playerChoice === "paper") || (computerChoice === "paper" && playerChoice === "scissor") || (computerChoice === "scissor" && playerChoice === "rock");
     if (computerChoice===playerChoice) return "tie";
@@ -19,71 +32,43 @@ function determineGameDecision(computerChoice,playerChoice){
     else if(!playerWin) return "lost";
 }
 
+function playGame(playerChoice){       
+    addToResultDiv("round "+currentRound);
+    let computerChoice=getComputerChoice();
+    addToResultDiv("Computer has chosen: "+computerChoice);        
+    addToResultDiv("Player has chosen: "+playerChoice);
+    let decision=determineGameDecision(computerChoice,playerChoice);
+    if (decision=="win"){
+        addToResultDiv("You won! "+ playerChoice+" beats "+computerChoice+"!");
+        playerPoints++;
+        currentRound++;
+    }else if(decision=="lost"){
+        addToResultDiv("You lost! "+ computerChoice+" beats "+playerChoice+"!");
+        computerPoints++;
+        currentRound++;
+    }else if(decision=="tie"){
+        addToResultDiv("Tie! Both chose "+playerChoice+"! Try again!:)");
+    }
 
-//TEST
-// let computerChoice=getComputerChoice();
-// let playerChoice=getPlayerChoice();
-// console.log(determineGameDecision(computerChoice,playerChoice));
-
-
-// SET rounds
-// FUNCTION playGame (rounds)
-function playGame(playerChoice){
-// SET currentRound to 1
-    let currentRound=1;
-// SET playerPoints to 0
-    let playerPoints=0;
-// SET computerPoints to 0
-    let computerPoints=0;
-// SET REPEAT to 0
-    let repeat=false;
-    do{
-        addToResultDiv("round "+currentRound);
-        let computerChoice=getComputerChoice();
-        addToResultDiv("Computer has chosen: "+computerChoice);        
-        addToResultDiv("Player has chosen: "+playerChoice);
-        let decision=determineGameDecision(computerChoice,playerChoice);
-        if (decision=="win"){
-            addToResultDiv("You won! "+ playerChoice+" beats "+computerChoice+"!");
-            playerPoints++;
-            currentRound++;
-            repeat=false;
-        }else if(decision=="lost"){
-            addToResultDiv("You lost! "+ computerChoice+" beats "+playerChoice+"!");
-            computerPoints++;
-            currentRound++;
-            repeat=false;
-        }else if(decision=="tie"){
-            addToResultDiv("Tie! Both chose "+playerChoice+"! Try again!:)");
-            repeat=true;
-        }
+    if (playerPoints<5){
         addToResultDiv("Score (Player:Computer) = "+playerPoints+":"+computerPoints);
-        }while(repeat==true);
-    addToResultDiv("Game Over");
-    // Deactivated, because currently only no limited number of rounds in implemented
-    // if(playerPoints>computerPoints){
-    //     addToResultDiv("Congratulations! You won the game with score "+playerPoints+":"+computerPoints);
-    // }else if(playerPoints<computerPoints){
-    //     addToResultDiv("You lost the game with score "+playerPoints+":"+computerPoints);
-    // }else if (playerPoints==computerPoints){
-    //     addToResultDiv("Tie! Score: "+playerPoints+":"+computerPoints);
-    // }
+    }else{
+        addToResultDiv("Game Over");
+        if(playerPoints>computerPoints){
+            addToResultDiv("Congratulations! You won the game with score "+playerPoints+":"+computerPoints);
+        }else if(playerPoints<computerPoints){
+            addToResultDiv("You lost the game with score "+playerPoints+":"+computerPoints);
+        }else if (playerPoints==computerPoints){
+            addToResultDiv("Tie! Score: "+playerPoints+":"+computerPoints);
+        }
+        currentRound=1;
+        playerPoints=0;
+        computerPoints=0;
+    } 
 }
 
-const buttons=document.querySelectorAll('button');
-buttons.forEach(button => {
-    button.addEventListener('click', btnEvent => {
-        playGame(btnEvent.target.id);
-    }) 
-});
 
-// const button_rock=document.querySelector('#rock');
-// const button_paper=document.querySelector('#paper');
-// const button_scissor=document.querySelector('#scissor');
 
-// button_rock.addEventListener('click', () => {playGame('rock')});
-// button_paper.addEventListener('click', () => {playGame('paper')});
-// button_scissor.addEventListener('click', () => {playGame('scissor')});
 
 const result_div=document.querySelector('#result_div');
 const addToResultDiv = (toAdd) => {
@@ -91,3 +76,6 @@ const addToResultDiv = (toAdd) => {
     child_div.textContent=toAdd;
     result_div.appendChild(child_div);
 }
+
+// Display the running score, and announce a winner of the game once one player reaches 5 points.
+
