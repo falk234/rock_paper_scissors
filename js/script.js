5d9866dd21dd1fd079da8917aff9ef52b23e653a
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-08 16:08:32 +0100

    Change code order for better readabilty

--- js/script.js
+++ js/script.js
@@ -4,34 +4,15 @@ let playerPoints=0;
 let computerPoints=0;
 let endPoints=5; // Once one player reaches x points, game will end
 
+// const button_rock=document.querySelector('#rock');
+// button_rock.addEventListener('click', () => {playGame('rock')});
+
 const buttons=document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', btnEvent => {
         playGame(btnEvent.target.id);
     }) 
 });
-
-// const button_rock=document.querySelector('#rock');
-// button_rock.addEventListener('click', () => {playGame('rock')});
-
-
-function getComputerChoice(){
-// COMPUTE random number 1-3
-    let number = Math.floor(Math.random() * 3) +1;
-// SET computerChoice to "rock", "paper", "scissor"
-    switch(number){
-        case 1: return "rock";
-        case 2: return "paper";
-        case 3: return "scissor";
-    }
-}
-
-function determineGameDecision(computerChoice,playerChoice){
-    let playerWin=(computerChoice === "rock" && playerChoice === "paper") || (computerChoice === "paper" && playerChoice === "scissor") || (computerChoice === "scissor" && playerChoice === "rock");
-    if (computerChoice===playerChoice) return "tie";
-    else if (playerWin) return "win";
-    else if(!playerWin) return "lost";
-}
 
 function playGame(playerChoice){       
     addToResultDiv("round "+currentRound);
@@ -68,9 +49,6 @@ function playGame(playerChoice){
     } 
 }
 
-
-
-
 const result_div=document.querySelector('#result_div');
 const addToResultDiv = (toAdd) => {
     const child_div=document.createElement('div');
@@ -78,5 +56,24 @@ const addToResultDiv = (toAdd) => {
     result_div.appendChild(child_div);
 }
 
-// Display the running score, and announce a winner of the game once one player reaches 5 points.
+function getComputerChoice(){
+// COMPUTE random number 1-3
+    let number = Math.floor(Math.random() * 3) +1;
+// SET computerChoice to "rock", "paper", "scissor"
+    switch(number){
+        case 1: return "rock";
+        case 2: return "paper";
+        case 3: return "scissor";
+    }
+}
+
+function determineGameDecision(computerChoice,playerChoice){
+    let playerWin=(computerChoice === "rock" && playerChoice === "paper") || (computerChoice === "paper" && playerChoice === "scissor") || (computerChoice === "scissor" && playerChoice === "rock");
+    if (computerChoice===playerChoice) return "tie";
+    else if (playerWin) return "win";
+    else if(!playerWin) return "lost";
+}
+
+
+
 
